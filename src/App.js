12e9bc4f0611940b9bf9bc3a8a597949c460bd05
EunchaeCho-SMU <eunchae01@naver.com>
2021-04-27 22:45:06 +0900

    프로젝트생성

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <ul>
+      <li><a href="usersearch.html">소환사 검색</a></li>
+		  <li><a href="championsearch.html">챔피언별 승률 검색</a></li>
+		  <li><a href="linesearch.html">라인별 승률 검색</a></li>
+		  <li><a href="complexsearch.html">조합 승률 검색</a></li>
+		  <li><a href="itemsearch.html">아이템 티어 순위 검색</a></li>
+		  <li><a href="leadersearch.html">리더보드</a></li>
+    </ul>
+
+
+  );
+}
+
+export default App;
