5e46ce22e4a54d699fc1b8eb15e5840182c276b6
EunchaeCho-SMU <eunchae01@naver.com>
2021-05-28 00:48:52 +0900

    로그value출력

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './input.css';
 
 function Input(){
+    const [input, setInputs] = useState({
+        name : ''
+    });
+
+    const {name} = input;
+
+    const onChange = (e) => {
+        console.log(e.target.name);
+        console.log(e.target.value);
+    };
+
     return(
         <div className='searchBox'>
-            <input className='inputBox' placeholder='소환사 이름을 입력해주세요.'/>
+            <input name='name' className='inputBox' placeholder='소환사 이름을 입력해주세요.' onChange={onChange}/>
             <button type='submit' className='searchBtn'>검색하기</button>
         </div>
     );
