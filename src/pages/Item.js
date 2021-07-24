273ddbf7fb0034fe1eebc974d4f675c0015267fd
donghwi <hanbundo17@gmail.com>
2021-07-25 07:17:19 +0900

    first commit

--- src/pages/Item.js
+++ src/pages/Item.js
@@ -12,6 +12,10 @@ function item(){
             <input className='inputBox' placeholder='챔피언 이름을 입력해주세요.'/>
             <button type='submit' className='searchBtn'>검색하기</button>
         </div>
+            <div className='searchBox'>
+            <input className='inputBox' placeholder='챔피언 이름을 입력해주세요.'/>
+            <button type='submit' className='searchBtn'>검색하기</button>
+        </div>
 
 
 
